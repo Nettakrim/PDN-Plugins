ebae99eac72a8cb4017df9b4c984d95614acc70a
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-12-30 20:24:19 +0000

    Added Color mode

--- EvenHistogram.cs
+++ EvenHistogram.cs
@@ -2,21 +2,20 @@
 // Submenu:
 // Author: Nettakrim
 // Title:
-// Version:
+// Version: 1.1
 // Desc: Evens out the histogram of the selected area
 // Keywords:
 // URL:
-// Help: CC0 - Public Domain
-
-// For help writing a GPU Image plugin: https://boltbait.com/pdn/CodeLab/help/tutorial/image/
+// Help: CC0 - Public Domain, when building make sure to enable Single Threaded and Single Render Call
 
 #region UICode
-IntSliderControl Seed = 0; // [0,1024] Seed for all the random decisions needed
 IntSliderControl MaxIterations = 10000; // [0,1000000] Amount of iterations to try solve for, set to 0 to go until solved
-CheckboxControl Quicker = true; // Massively speed up solving by smoothing more than just the global maximum. results may be less accurate
+IntSliderControl BonusSteps = 256; // [0,256] Extra steps per iteration, massively speeds up solving, but introduces more noise
+CheckboxControl Grayscale = true; // Grayscale, using red channel
+IntSliderControl Seed = 0; // [0,1024] Seed for all the random decisions needed
 #endregion
 
-public class Bar
+private class Bar
 {
     private int[] values;
     private int amount;
@@ -82,6 +81,7 @@ public class Bar
 
     public int ClaimValue(Random random)
     {
+        // get index from weighted random, used to apply the final values with the transposed histogram
         int choice = random.Next(amount);
         amount--;
 
@@ -99,7 +99,7 @@ public class Bar
     }
 }
 
-public void Shift(Bar[] bars, int index, Random random) {
+private void Shift(Bar[] bars, int index, Random random) {
     // get difference needed for each side
     int amount = bars[index].GetAmount();
     int lowerDelta = index == 0 ? 0 : Math.Max(0, amount - bars[index - 1].GetAmount());
@@ -130,40 +130,7 @@ public void Shift(Bar[] bars, int index, Random random) {
     bars[index].MoveCountTo(bars[destination], destination, shift, random);
 }
 
-protected override void OnRender(IBitmapEffectOutput output)
-{
-    using IEffectInputBitmap<ColorBgra32> sourceBitmap = Environment.GetSourceBitmapBgra32();
-    using IBitmapLock<ColorBgra32> sourceLock = sourceBitmap.Lock(new RectInt32(0, 0, sourceBitmap.Size));
-        RegionPtr<ColorBgra32> sourceRegion = sourceLock.AsRegionPtr();
-
-    RectInt32 outputBounds = output.Bounds;
-    using IBitmapLock<ColorBgra32> outputLock = output.LockBgra32();
-        RegionPtr<ColorBgra32> outputSubRegion = outputLock.AsRegionPtr();
-    var outputRegion = outputSubRegion.OffsetView(-outputBounds.Location);
-    //uint seed = RandomNumber.InitializeSeed(RandomNumberRenderSeed, outputBounds.Location);
-
-    var selection = Environment.Selection.RenderBounds;
-
-    Random random = new Random(Seed);
-
-    // initialise histogram
-    Bar[] bars = new Bar[256];
-    for (int i = 0; i < 256; i++)
-    {
-        bars[i] = new Bar();
-    }
-
-
-    // set histogram values
-    for (int y = selection.Top; y < selection.Bottom; ++y)
-    {
-        for (int x = selection.Left; x < selection.Right; ++x)
-        {
-            ColorBgra32 sourcePixel = sourceRegion[x, y];
-            bars[sourcePixel.R].Increment(sourcePixel.R, 1);
-        }
-    }
-
+private Bar[] Solve(Bar[] bars, Random random) {
     // iteratively diffuse histogram
     for (int x = 0; x < MaxIterations || MaxIterations == 0; x++)
     {
@@ -198,55 +165,109 @@ protected override void OnRender(IBitmapEffectOutput output)
 
         Shift(bars, maxIndex, random);
 
-        if (Quicker) {
-            int shuffleA = random.Next(256);
-            int shuffleB = random.Next(256);
-            for (int i = 0; i < 256; i++)
-            {
-                Shift(bars, ((i ^ shuffleA)+shuffleB)&255, random);
-            }
+        int shuffleA = random.Next(256);
+        int shuffleB = random.Next(256);
+        for (int i = 0; i < BonusSteps; i++)
+        {
+            Shift(bars, ((i ^ shuffleA)+shuffleB)&255, random);
         }
     }
 
     // transpose histogram, so that each bar contains all the information needed for the weighted random
     Bar[] transposed = new Bar[256];
-    for (int i = 0; i < 256; i++)
-    {
-        transposed[i] = new Bar();
-    }
 
     for (int i = 0; i < 256; i++)
     {
+        transposed[i] = new Bar();
         for (int j = 0; j < 256; j++)
         {
             transposed[i].Increment(j, bars[j].GetValue(i));
         }
     }
 
-    #if DEBUG
-    Debug.WriteLine("applying changes "+Random.Shared.Next());
-    #endif
+    return transposed;
+}
 
-    // apply changes - for each pixel, set color to the index from a weighted random of all the histograms that have that value
+private Bar[] GetBars(RectInt32 selection, RegionPtr<ColorBgra32> sourceRegion, int index) {
+    // initialise histogram
+    Bar[] bars = new Bar[256];
+    for (int i = 0; i < 256; i++)
+    {
+        bars[i] = new Bar();
+    }
+
+    // set histogram values
     for (int y = selection.Top; y < selection.Bottom; ++y)
     {
         for (int x = selection.Left; x < selection.Right; ++x)
         {
             ColorBgra32 sourcePixel = sourceRegion[x, y];
-            int index = sourcePixel.R;
+            bars[sourcePixel[index]].Increment(sourcePixel[index], 1);
+        }
+    }
 
-            // set destination to weighted random
-            byte newValue = (byte)(transposed[sourcePixel.R].ClaimValue(random));
-            sourcePixel.R = newValue;
-            sourcePixel.G = newValue;
-            sourcePixel.B = newValue;
-            try
+    return bars;
+}
+
+protected override void OnRender(IBitmapEffectOutput output)
+{
+    using IEffectInputBitmap<ColorBgra32> sourceBitmap = Environment.GetSourceBitmapBgra32();
+    using IBitmapLock<ColorBgra32> sourceLock = sourceBitmap.Lock(new RectInt32(0, 0, sourceBitmap.Size));
+        RegionPtr<ColorBgra32> sourceRegion = sourceLock.AsRegionPtr();
+
+    RectInt32 outputBounds = output.Bounds;
+    using IBitmapLock<ColorBgra32> outputLock = output.LockBgra32();
+        RegionPtr<ColorBgra32> outputSubRegion = outputLock.AsRegionPtr();
+    var outputRegion = outputSubRegion.OffsetView(-outputBounds.Location);
+
+    var selection = Environment.Selection.RenderBounds;
+
+    Random random = new Random(Seed);
+
+    if (Grayscale) {
+        // colors are ordered bgra, so r is index 2
+        Bar[] bars = Solve(GetBars(selection, sourceRegion, 2), random);
+
+        for (int y = selection.Top; y < selection.Bottom; ++y)
+        {
+            for (int x = selection.Left; x < selection.Right; ++x)
             {
-                outputRegion[x, y] = sourcePixel;
+                ColorBgra32 sourcePixel = sourceRegion[x, y];
+                byte newValue = (byte)(bars[sourcePixel[2]].ClaimValue(random));
+                sourcePixel.R = newValue;
+                sourcePixel.G = newValue;
+                sourcePixel.B = newValue;
+
+                // why does this cause errors? i think its due to the multi threading
+                try
+                {
+                    outputRegion[x, y] = sourcePixel;
+                }
+                catch {}
             }
-            catch
-            {
+        }
+    }
+    else
+    {
+        Bar[] r = Solve(GetBars(selection, sourceRegion, 2), random);
+        Bar[] g = Solve(GetBars(selection, sourceRegion, 1), random);
+        Bar[] b = Solve(GetBars(selection, sourceRegion, 0), random);
 
+        for (int y = selection.Top; y < selection.Bottom; ++y)
+        {
+            for (int x = selection.Left; x < selection.Right; ++x)
+            {
+                ColorBgra32 sourcePixel = sourceRegion[x, y];
+                sourcePixel.R = (byte)(r[sourcePixel.R].ClaimValue(random));
+                sourcePixel.G = (byte)(g[sourcePixel.G].ClaimValue(random));
+                sourcePixel.B = (byte)(b[sourcePixel.B].ClaimValue(random));
+
+                // why does this cause errors? i think its due to the multi threading
+                try
+                {
+                    outputRegion[x, y] = sourcePixel;
+                }
+                catch {}
             }
         }
     }
